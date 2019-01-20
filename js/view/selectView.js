12ded23174facda963d80ade4ec79c10dbde76c7
Fadhilmch <fadhil.mochammad1095@gmail.com>
2019-01-20 20:30:47 +0100

    updated selectdish.html

--- js/view/selectView.js
+++ js/view/selectView.js
@@ -53,11 +53,18 @@ var SelectView = function (container, model) {
       );
   });
 
-  for(var i = 0; i < arrDishes.length; i++){
-    dishType.append(`<li><a href="#">${arrDishes[i]}</a></li>`);
+  // for(var i = 0; i < arrDishes.length; i++){
+  //   dishType.append(`<option value=${arrDishes[i].toLowerCase()}>${arrDishes[i]}</option>`);
+	// }
+	
+	for(var i = 0; i < arrDishes.length; i++){
+    dishType.append(`<li class="dropdown-item">${arrDishes[i]}</a></li>`);
   }
 
 
+	// Total Cost
+	var totalCost = container.find('#totalCost');
+	totalCost.html(`SEK 0.00`)
   console.log(arrDishes);
 
 	/**
