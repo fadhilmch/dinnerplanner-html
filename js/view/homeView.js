2f29bf16a179de18ee600e87d22ec3c4b3db8d68
Fadhilmch <fadhil.mochammad1095@gmail.com>
2019-01-21 17:58:05 +0100

    merge

--- js/view/homeView.js
+++ js/view/homeView.js
@@ -33,16 +33,31 @@ var HomeView = function (container, model) {
 	 * in some other view gives the same ID to another element.
 	 * 
 	 */
+	
+	// Dummy 
+	model.addDishToMenu(1);
+	model.addDishToMenu(100);
 
-  // Select option for guests number
+	 // Get from model
+	var arrDishes = model.getDishType();
+	var allDishes = model.getAllDishes();
+	var allMenu = model.getFullMenu();
+	var totalPrice = model.getTotalMenuPrice();
+	var totalGuests = model.getNumberOfGuests();
+
+	// ===========================
+	// 			selectdish.html 
+	// ===========================
+
+	// Select option for guests number
   var numberOfGuests = container.find(".guest");
   for (var i = 1; i < 10; i++){
     numberOfGuests.append(`<option value="${i}" ${(i===1)?"selected":""}>${i}</option>`);
-  }
+	}
+
 
   // Dropdown Select for Dishes Type
   var dishType = container.find('#dishType');
-  var arrDishes = model.getDishType();
   arrDishes.splice(0,0,'all');
   arrDishes = arrDishes.map(dish => {
       return dish.replace(
@@ -51,37 +66,127 @@ var HomeView = function (container, model) {
               return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
           }
       );
-  });
-
-  // for(var i = 0; i < arrDishes.length; i++){
-  //   dishType.append(`<option value=${arrDishes[i].toLowerCase()}>${arrDishes[i]}</option>`);
-	// }
+	});
 	
-	for(var i = 0; i < arrDishes.length; i++){
-    dishType.append(`<li class="dropdown-item">${arrDishes[i]}</a></li>`);
-  }
-
-
 	// Total Cost
-	var totalCost = container.find('.totalCost');
-	totalCost.html(`SEK 0.00`)
-	console.log(arrDishes);
-	
-	// Menu Wrapper
-	var menuWrapper = container.find('#menu-wrapper');
-	var allDishes = model.getAllDishes();
-	allDishes.forEach(dish => {
-		menuWrapper.append(`
-			<div class="col-sm-6 col-md-3 col-lg-2">
-				<div class="menu">
-					<img src="images/${dish.image}" alt="${dish.name}">
-					<div class="caption">
-						<h5>${dish.name}</h5>
+	var updateTotalCost = function(){
+		totalPrice = model.getTotalMenuPrice();
+		var totalCost = container.find('.totalCost');
+		totalCost.html(`SEK ${Number(totalPrice).toFixed(2)}`);
+		
+		// Menu Wrapper
+		var menuWrapper = container.find('#menu-wrapper');
+		allDishes.forEach(dish => {
+			menuWrapper.append(`
+				<div class="col-sm-6 col-md-3 col-lg-2">
+					<div class="menu">
+						<img src="images/${dish.image}" alt="${dish.name}">
+						<div class="caption">
+							<h5>${dish.name}</h5>
+						</div>
 					</div>
 				</div>
-			</div>
-		`)
+			`)
+		})
+	}
+
+	updateTotalCost();
+
+	// Confirm button
+	if(allMenu.length > 0){
+		var confirmButton = container.find('.btn-confirm');
+		confirmButton.prop('disabled', false);
+	}
+
+	// Table Sidebar
+	var updateSidebar = function() {
+		var menuTable = container.find('.menu-table');
+		allMenu = model.getFullMenu();
+		totalGuests = model.getNumberOfGuests();
+		menuTable.children().remove();
+		allMenu.forEach(dish => {
+			menuTable.append(`							
+				<tr>
+					<td>${dish.name}</td>
+					<td>${
+						Number(
+							dish.ingredients.map(ingredient => {
+								return ingredient.quantity * ingredient.price;
+							})
+							.reduce((acc, cur) => {
+								return acc + cur;
+							})
+						).toFixed(2)*totalGuests
+					}
+					</td>
+				</tr>
+			`);
+		})
+	}
+	updateSidebar();
+
+
+
+	// ======> EVENT 
+
+	// Change nummber of guess
+	numberOfGuests.on('change', function() {
+		console.log(`Number of guests: ${this.value}`);
+		model.setNumberOfGuests(this.value);
+		updateTotalCost();
+		updateSidebar();
 	})
+
+  // // Select option for guests number
+  // var numberOfGuests = container.find(".guest");
+  // for (var i = 1; i < 10; i++){
+  //   numberOfGuests.append(`<option value="${i}" ${(i===1)?"selected":""}>${i}</option>`);
+  // }
+
+  // // Dropdown Select for Dishes Type
+  // var dishType = container.find('#dishType');
+  // var arrDishes = model.getDishType();
+  // arrDishes.splice(0,0,'all');
+  // arrDishes = arrDishes.map(dish => {
+  //     return dish.replace(
+  //         /\w\S*/g,
+  //         function(txt) {
+  //             return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  //         }
+  //     );
+  // });
+
+  // // for(var i = 0; i < arrDishes.length; i++){
+  // //   dishType.append(`<option value=${arrDishes[i].toLowerCase()}>${arrDishes[i]}</option>`);
+	// // }
+	
+	// for(var i = 0; i < arrDishes.length; i++){
+  //   dishType.append(`<li class="dropdown-item">${arrDishes[i]}</a></li>`);
+  // }
+
+
+	// // Total Cost
+	// var totalCost = container.find('.totalCost');
+	// totalCost.html(`SEK 0.00`)
+	// console.log(arrDishes);
+	
+	// // Menu Wrapper
+	// var menuWrapper = container.find('#menu-wrapper');
+	// var allDishes = model.getAllDishes();
+	// allDishes.forEach(dish => {
+	// 	menuWrapper.append(`
+	// 		<div class="col-sm-6 col-md-3 col-lg-2">
+	// 			<div class="menu">
+	// 				<img src="images/${dish.image}" alt="${dish.name}">
+	// 				<div class="caption">
+	// 					<h5>${dish.name}</h5>
+	// 				</div>
+	// 			</div>
+	// 		</div>
+	// 	`)
+	// })
+
+	// ================================================
 
 	// dish detail
 
--- js/view/selectView.js
+++ js/view/selectView.js
@@ -34,257 +34,119 @@ var SelectView = function (container, model) {
 	 * 
 	 */
 	
-	 // Dummy
-	model.addDishToMenu(1);
-	model.addDishToMenu(100);
-	var tes = container.find('#tes');
-	// tes.load('./components/title.html');
-	$("#tes").load("./components/title.html #insert");
-	// $("#tes").append("./components/title.html");
+// 	 // Dummy
+// 	model.addDishToMenu(1);
+// 	model.addDishToMenu(100);
+// 	var tes = container.find('#tes');
+// 	// tes.load('./components/title.html');
+// 	$("#tes").load("./components/title.html #insert");
+// 	// $("#tes").append("./components/title.html");
 
 	
-	// Get from model
-	var arrDishes = model.getDishType();
-	var allDishes = model.getAllDishes();
-	var allMenu = model.getFullMenu();
-	var totalPrice = model.getTotalMenuPrice();
-	var totalGuests = model.getNumberOfGuests();
+// 	// Get from model
+// 	var arrDishes = model.getDishType();
+// 	var allDishes = model.getAllDishes();
+// 	var allMenu = model.getFullMenu();
+// 	var totalPrice = model.getTotalMenuPrice();
+// 	var totalGuests = model.getNumberOfGuests();
 	
 
-<<<<<<< HEAD
- //  // Select option for guests number
- //  var numberOfGuests = container.find(".guest");
- //  for (var i = 1; i < 10; i++){
- //    numberOfGuests.append(`<option value="${i}" ${(i===1)?"selected":""}>${i}</option>`);
- //  }
-
- //  // Dropdown Select for Dishes Type
- //  var dishType = container.find('#dishType');
- //  var arrDishes = model.getDishType();
- //  arrDishes.splice(0,0,'all');
- //  arrDishes = arrDishes.map(dish => {
- //      return dish.replace(
- //          /\w\S*/g,
- //          function(txt) {
- //              return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
- //          }
- //      );
- //  });
-
- //  // for(var i = 0; i < arrDishes.length; i++){
- //  //   dishType.append(`<option value=${arrDishes[i].toLowerCase()}>${arrDishes[i]}</option>`);
-	// // }
-=======
-  // Select option for guests number
-  var numberOfGuests = container.find(".guest");
-  for (var i = 1; i < 10; i++){
-    numberOfGuests.append(`<option value="${i}" ${(i===1)?"selected":""}>${i}</option>`);
-	}
+//   // Select option for guests number
+//   var numberOfGuests = container.find(".guest");
+//   for (var i = 1; i < 10; i++){
+//     numberOfGuests.append(`<option value="${i}" ${(i===1)?"selected":""}>${i}</option>`);
+// 	}
 
 
-  // Dropdown Select for Dishes Type
-  var dishType = container.find('#dishType');
-  arrDishes.splice(0,0,'all');
-  arrDishes = arrDishes.map(dish => {
-      return dish.replace(
-          /\w\S*/g,
-          function(txt) {
-              return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-          }
-      );
-  });
+//   // Dropdown Select for Dishes Type
+//   var dishType = container.find('#dishType');
+//   arrDishes.splice(0,0,'all');
+//   arrDishes = arrDishes.map(dish => {
+//       return dish.replace(
+//           /\w\S*/g,
+//           function(txt) {
+//               return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+//           }
+//       );
+//   });
 
-  // for(var i = 0; i < arrDishes.length; i++){
-  //   dishType.append(`<option value=${arrDishes[i].toLowerCase()}>${arrDishes[i]}</option>`);
-	// }
->>>>>>> c336a1f5e9a3592de77fb3dd1d4e93bf595f5a31
+//   // for(var i = 0; i < arrDishes.length; i++){
+//   //   dishType.append(`<option value=${arrDishes[i].toLowerCase()}>${arrDishes[i]}</option>`);
+// 	// }
 	
-	// for(var i = 0; i < arrDishes.length; i++){
- //    dishType.append(`<li class="dropdown-item">${arrDishes[i]}</a></li>`);
- //  }
+// 	// for(var i = 0; i < arrDishes.length; i++){
+//  //    dishType.append(`<li class="dropdown-item">${arrDishes[i]}</a></li>`);
+//  //  }
 
 
-<<<<<<< HEAD
-	// // Total Cost
-	// var totalCost = container.find('.totalCost');
-	// totalCost.html(`SEK 0.00`)
-	// console.log(arrDishes);
-	
-	// // Menu Wrapper
-	// var menuWrapper = container.find('#menu-wrapper');
-	// var allDishes = model.getAllDishes();
-	// allDishes.forEach(dish => {
-	// 	menuWrapper.append(`
-	// 		<div class="col-sm-6 col-md-3 col-lg-2">
-	// 			<div class="menu">
-	// 				<img src="images/${dish.image}" alt="${dish.name}">
-	// 				<div class="caption">
-	// 					<h5>${dish.name}</h5>
-	// 				</div>
-	// 			</div>
-	// 		</div>
-	// 	`)
-	// })
-
-	// // dish detail
-
-	// var detailDish = container.find("#dish-wrapper");
- //  	var getDish = model.getDish(2);
- //  	console.log(getDish);
- //  	detailDish.append(`<div>
-	// 				<h4>${getDish.name}</h4>
-					
-	// 				  	<img class="fitImage" alt="Responsive image" src="images/${getDish.image}">
-	// 					<div>
-	// 				  		<p>${getDish.description} </p>  </div>
-	// 				  		<a href="#" class="btn btn-warning">Back to Search</a>
-	// 				</div>
-	// 			</div>`)
-
- //  	var ingredientsDish = container.find("#ingredients-wrapper");
-
- //  	getDish.ingredients.forEach(dish => {
- //  	ingredientsDish.append(`<div class="table-responsive">
-	// 			<table class="table">
-	// 				  <tbody> 	
-	// 				    <tr>
-	// 				      <th scope="row">${dish.quantity + dish.unit}</th>
-	// 				      <td>${dish.name}</td>
-	// 				      <td>SEK</td>
-	// 				      <td>${dish.price}</td>
-	// 				    </tr>
-			
-	// 				  </tbody>
-	// 			</table>
-	// 			</div>`)
- //  })
-  	
-	// //selected menu on over view
-
-	// model.addDishToMenu(1);
-	// model.addDishToMenu(2);
-	// model.addDishToMenu(103);
-
-	// var selectedMenu = container.find("#selected-wrapper");
-	// var allMenu = model.getFullMenu();
-	// console.log(allMenu);
-	// allMenu.forEach(dish => {
-	// 	// $("#selected-wrapper").append(('<p>Test</p>'))
-	// 	// selectedMenu.append('<p>Test</p>');
-
-	// 	selectedMenu.append(`
-	// 		<div class="col-sm-6 col-md-3">
-	// 					<div class="img-thumbnail">
-	// 						<img src="images/${dish.image}" alt="...">
-	// 						<div class="caption">
-	// 							<h6 style="padding-top: 5px">${dish.name}</h6>
-
-	// 						</div>
-	// 					</div>
-	// 					<div class="caption" style="padding-top: 5px">
-	// 						<h6 class="text-danger" style="text-align: right;"> SEK</h6>
-	// 					</div>
-	// 				</div>
-	// 	`)
-	// })
-
-	// model.setNumberOfGuests(2);
-
-	// console.log(model.getNumberOfGuests());
-	// var priceAcc = container.find("#totalPrice");
-	// var totalPrice = model.getTotalMenuPrice();
-	// priceAcc.append(`
-	// 	${totalPrice}`);
-
-
-=======
-	// Total Cost
-	var updateTotalCost = function(){
-		totalPrice = model.getTotalMenuPrice();
-		var totalCost = container.find('.totalCost');
-		totalCost.html(`SEK ${Number(totalPrice).toFixed(2)}`);
+// 	// Total Cost
+// 	var updateTotalCost = function(){
+// 		totalPrice = model.getTotalMenuPrice();
+// 		var totalCost = container.find('.totalCost');
+// 		totalCost.html(`SEK ${Number(totalPrice).toFixed(2)}`);
 		
-		// Menu Wrapper
-		var menuWrapper = container.find('#menu-wrapper');
-		allDishes.forEach(dish => {
-			menuWrapper.append(`
-				<div class="col-sm-6 col-md-3 col-lg-2">
-					<div class="menu">
-						<img src="images/${dish.image}" alt="${dish.name}">
-						<div class="caption">
-							<h5>${dish.name}</h5>
-						</div>
-					</div>
-				</div>
-			`)
-		})
-	}
+// 		// Menu Wrapper
+// 		var menuWrapper = container.find('#menu-wrapper');
+// 		allDishes.forEach(dish => {
+// 			menuWrapper.append(`
+// 				<div class="col-sm-6 col-md-3 col-lg-2">
+// 					<div class="menu">
+// 						<img src="images/${dish.image}" alt="${dish.name}">
+// 						<div class="caption">
+// 							<h5>${dish.name}</h5>
+// 						</div>
+// 					</div>
+// 				</div>
+// 			`)
+// 		})
+// 	}
 
-	updateTotalCost();
+// 	updateTotalCost();
 
-	// Confirm button
-	if(allMenu.length > 0){
-		var confirmButton = container.find('.btn-confirm');
-		confirmButton.prop('disabled', false);
-	}
+// 	// Confirm button
+// 	if(allMenu.length > 0){
+// 		var confirmButton = container.find('.btn-confirm');
+// 		confirmButton.prop('disabled', false);
+// 	}
 
-	// Table Sidebar
-	var updateSidebar = function() {
-		var menuTable = container.find('.menu-table');
-		allMenu = model.getFullMenu();
-		totalGuests = model.getNumberOfGuests();
-		menuTable.children().remove();
-		allMenu.forEach(dish => {
-			menuTable.append(`							
-				<tr>
-					<td>${dish.name}</td>
-					<td>${
-						Number(
-							dish.ingredients.map(ingredient => {
-								return ingredient.quantity * ingredient.price;
-							})
-							.reduce((acc, cur) => {
-								return acc + cur;
-							})
-						).toFixed(2)*totalGuests
-					}
-					</td>
-				</tr>
-			`);
-		})
-	}
-	updateSidebar();
->>>>>>> c336a1f5e9a3592de77fb3dd1d4e93bf595f5a31
+// 	// Table Sidebar
+// 	var updateSidebar = function() {
+// 		var menuTable = container.find('.menu-table');
+// 		allMenu = model.getFullMenu();
+// 		totalGuests = model.getNumberOfGuests();
+// 		menuTable.children().remove();
+// 		allMenu.forEach(dish => {
+// 			menuTable.append(`							
+// 				<tr>
+// 					<td>${dish.name}</td>
+// 					<td>${
+// 						Number(
+// 							dish.ingredients.map(ingredient => {
+// 								return ingredient.quantity * ingredient.price;
+// 							})
+// 							.reduce((acc, cur) => {
+// 								return acc + cur;
+// 							})
+// 						).toFixed(2)*totalGuests
+// 					}
+// 					</td>
+// 				</tr>
+// 			`);
+// 		})
+// 	}
+// 	updateSidebar();
 
-	/**
-	 * When we want references to some view elements to be available from outside of view, we 
-	 * define them as this.someName. We don't need this in Lab 1 yet, but in Lab 2 it 
-	 * will be important for assigning listeners to these buttons, because the listeners
-	 * should not be assigned in the view, but rather in controller.
-	 * 
-	 * We can then, in some other code, use exampleView.plusButton to reference the 
-	 * this button and do something with it (see Lab 2).
-	 * 
-	 */
-	this.plusButton = container.find("#plusGuest");
-	this.minusButton = container.find("#minusGuest");
+
+
+// 	// ======> EVENT 
+
+// 	// Change nummber of guess
+// 	numberOfGuests.on('change', function() {
+// 		console.log(`Number of guests: ${this.value}`);
+// 		model.setNumberOfGuests(this.value);
+// 		updateTotalCost();
+// 		updateSidebar();
+// 	})
 	
-	/**
-	 * Here we use @var {jQuery object} numberOfGuests that is a reference to <span>
-	 * in our view to dynamically set it's value to "Hello World".
-	 */
-	
-
-
-	// ======> EVENT 
-
-	// Change nummber of guess
-	numberOfGuests.on('change', function() {
-		console.log(`Number of guests: ${this.value}`);
-		model.setNumberOfGuests(this.value);
-		updateTotalCost();
-		updateSidebar();
-	})
-	
-}
+// }
  
