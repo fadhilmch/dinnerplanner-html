d6dfb5bac2dcdcf49a2b56ef868f0000440324b7
Fadhilmch <fadhil.mochammad1095@gmail.com>
2019-01-22 14:40:27 +0100

    finished selectdish.html

--- js/view/searchView.js
+++ js/view/searchView.js
@@ -3,44 +3,68 @@ var SearchView = function (container, model) {
   // Get from model
   var allDishes = model.getAllDishes();
   var arrDishes = model.getDishType();
+  var allMenu = model.getFullMenu();
 
   // Initialize Component
   var dishType = container.find('#dishType');
   var menuWrapper = container.find('#menu-wrapper');
   var searchButton = container.find('#search-btn');
   var searchInput = container.find('search-input');
+  var searchTitle = container.find('#search-title');
+
+  // Initialize function
+  var initialize = function() {
+    showDropdownType();
+    setSearchTitle();
+  }
 
   // Dropdown Select for Dishes Type
-  arrDishes.splice(0, 0, 'all');
-  arrDishes = arrDishes.map(dish => {
-    return dish.replace(
-      /\w\S*/g,
-      function (txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-      }
-    );
-  });
-
-  // for(var i = 0; i < arrDishes.length; i++){
-  //   dishType.append(`<option value=${arrDishes[i].toLowerCase()}>${arrDishes[i]}</option>`);
-  // }
-
-  for(var i = 0; i < arrDishes.length; i++){
-     dishType.append(`<li class="dropdown-item">${arrDishes[i]}</a></li>`);
-   }
+  var showDropdownType = function() {
+    arrDishes.splice(0, 0, 'all');
+    arrDishes = arrDishes.map(dish => {
+      return dish.replace(
+        /\w\S*/g,
+        function (txt) {
+          return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+      );
+    });
+    // for(var i = 0; i < arrDishes.length; i++){
+    //   dishType.append(`<option value=${arrDishes[i].toLowerCase()}>${arrDishes[i]}</option>`);
+    // }
+  
+    for(var i = 0; i < arrDishes.length; i++){
+       dishType.append(`<li class="dropdown-item">${arrDishes[i]}</a></li>`);
+     }
+  }
 
   // Menu Wrapper
-  allDishes.forEach(dish => {
-    menuWrapper.append(`
-				<div class="col-sm-6 col-md-3 col-lg-2">
-					<div class="menu">
-						<img src="images/${dish.image}" alt="${dish.name}">
-						<div class="caption">
-							<h5>${dish.name}</h5>
-						</div>
-					</div>
-				</div>
-			`)
-  })
+  var showDishesChoice = function() {
+    allDishes.forEach(dish => {
+      menuWrapper.append(`
+          <div class="col-sm-6 col-md-3 col-lg-2">
+            <div class="menu">
+              <img src="images/${dish.image}" alt="${dish.name}">
+              <div class="caption">
+                <h5>${dish.name}</h5>
+              </div>
+            </div>
+          </div>
+        `)
+    })
+  }
+
+
+  var setSearchTitle = function() {
+    console.log('fdlsfj')
+    searchTitle.html('Find a Dish');
+    if(allMenu.length===0)
+      searchTitle.html('Find a Dish');
+    else
+      searchTitle.html('Add another dish');
+  }
+
+  initialize();
+  showDishesChoice();
 }
 
